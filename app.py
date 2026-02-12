6debc1b79d2ced9266210798251a61e10de75579
RuanJengYi <j4101441014@gmail.com>
2026-02-12 14:39:37 +0800

    Update and rename app.py to test_app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("---------------------------------------")
-    print(" 🚀 App is starting inside Docker...")
-    print(" 📂 Reading file from container...")
-    print(" ✅ Environment is ready!")
-    print("---------------------------------------")
-
-if __name__ == "__main__":
-    main()
---
+++ test_app.py
@@ -0,0 +1,10 @@
+import pytest
+
+def add(a, b):
+    return a+b
+
+def test_add_pass():
+    assert add(1, 1) == 2
+
+def test_add_fail():
+    assert add(1, 1) == 4
